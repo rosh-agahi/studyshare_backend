99d4eab0247994197ad9af2385caf7e0277f3155
rosh-agahi <rosh.agahi@gmail.com>
2021-06-27 22:58:34 -0700

    add flashcard resource to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :subjects, only: [:index, :create]
+      resources :flashcards, only: [:index, :create]
     end
   end
 end
