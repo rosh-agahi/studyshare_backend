40da313087f674a1fb687d8f53a50fd5a6640a53
Roshanak <rosh.agahi@gmail.com>
2021-07-10 16:58:21 -0700

    add user resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :subjects, only: [:index, :create]
       resources :flashcards, only: [:index, :create]
+      resources :users, only:[:index, :create]
     end
   end
 end
